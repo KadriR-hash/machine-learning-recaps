b034e5415f10336f1447c0a8c050d5985eb5d416
KadriR-hash <kadriraed11@gmail.com>
2022-05-30 23:43:35 +0100

    training and predicting

--- Linear Regression/test1.py
+++ Linear Regression/test1.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+from matplotlib import pyplot as plt
 import tensorflow as tf
 
 # x = [1, 2, 2.5, 3, 4]
@@ -42,3 +42,40 @@ for feature_name in NUMERIC_COLUMNS:
     feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
 print(feature_columns)
+
+
+# The Training Process
+
+# Input function
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():  # inner function, this will be returned
+        # create tf.data.Dataset object with data and label
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)  # randomize order of data
+        # split dataset into batches of 32 and repeat process for number of epochs
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds  # return a batch of the dataset
+
+    return input_function  # return a function object for use
+
+
+# here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# Creating the Model ( linear regression algorithm)
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+# We create a linear estimtor by passing the feature columns we created earlier
+
+# Training the Model
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on testing data
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model (0.74242425)
+
+# Predict using the eval  data
+pred_dicts = list(linear_est.predict(eval_input_fn))
+probs = pd.Series([pred['probabilities'][1] for pred in pred_dicts])
+
+probs.plot(kind='hist', bins=20, title='predicted probabilities')
+plt.show()
