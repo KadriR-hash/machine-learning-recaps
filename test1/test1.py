0b53aaf39030b293cfa4b14ee8df229fdfad2da4
KadriR-hash <kadriraed11@gmail.com>
2022-05-30 23:22:23 +0100

    features-columns

--- test1/test1.py
+++ test1/test1.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 # Load dataset.
 dftrain = pd.read_csv('train.csv')  # training data
 dfeval = pd.read_csv('eval.csv')  # testing data
-y_train = dftrain.pop('survived')
+y_train = dftrain.pop('survived')  # removing the survived column
 y_eval = dfeval.pop('survived')
 
 print(dftrain.head())  # This will show us the first 5 items in our dataframe.
@@ -27,3 +27,18 @@ print(y_train.head())
 # dftrain.sex.value_counts().plot(kind='barh')
 # dftrain['class'].value_counts().plot(kind='barh')
 # pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+
+# Before we continue and create/train a model we must convet our categorical data into numeric data.
+# We can do this by encoding each category with an integer (ex. male = 1, female = 2).
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
