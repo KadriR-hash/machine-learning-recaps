a0acd77e43a869de65a1a996b2de44b17e54e45e
KadriR-hash <kadriraed11@gmail.com>
2022-05-30 23:12:37 +0100

    some code cleaning

--- test1/test1.py
+++ test1/test1.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import tensorflow as tf
 
 # x = [1, 2, 2.5, 3, 4]
@@ -18,13 +18,12 @@ dfeval = pd.read_csv('eval.csv')  # testing data
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 
-print(dftrain.head())
-print(dftrain.describe())
+print(dftrain.head())  # This will show us the first 5 items in our dataframe.
+print(dftrain.describe())  # statistical analysis of our data
 print(dftrain.shape)
 print(y_train.head())
 
-#dftrain.age.hist(bins=20)
-#dftrain.sex.value_counts().plot(kind='barh')
-
-#dftrain['class'].value_counts().plot(kind='barh')
-#pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+# dftrain.age.hist(bins=20)
+# dftrain.sex.value_counts().plot(kind='barh')
+# dftrain['class'].value_counts().plot(kind='barh')
+# pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
